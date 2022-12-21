26310a20836d177d6a595b72773c0b9fb85d334c
mhsegval <mohammedsegval53@gmail.com>
2022-12-21 10:11:21 -0500

    project setup and intro page added

---
+++ src/components/work/Work.jsx
@@ -0,0 +1,24 @@
+import "./work.scss"
+
+const Work = () => {
+  return (
+    <div className="work" id="work">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                
+              </div>
+            </div>
+            <div className="right">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Work 
